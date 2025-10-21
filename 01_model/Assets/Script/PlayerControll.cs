4419761acf9cb088da1a71e472a7051f76542a88
abiraj_karthik_rajan <abiraj.karthikrajan@stud.h-da.de>
2025-10-21 20:39:03 +0530

    2. Allow the vehicle to move left/right

---
+++ 01_model/Assets/Script/PlayerControll.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed = 5.0f;
+    public float horizontalInput;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      horizontalInput = Input.GetAxis("Horizontal");
+      
+      // we'll move the vehical forward
+      transform.Translate(Vector3.forward * Time.deltaTime * speed);
+      transform.Translate(Vector3.right*Time.deltaTime * turnSpeed * horizontalInput);
+      
+    }
+}
